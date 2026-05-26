40fdba4af0ae8a83a7da3255859b75c1a8b02be5
kiel997 <ezekieludoe3@gmail.com>
2026-05-26 13:26:32 +0300

    fix auth

--- backend/STUDENTS_ASSIGNMENTS/week6/ezekiel/src/auth/providers/sign-in-provider.ts
+++ backend/STUDENTS_ASSIGNMENTS/week6/ezekiel/src/auth/providers/sign-in-provider.ts
@@ -1,7 +1,14 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
 import { UsersService } from 'src/users/users.service';
+
 import { HashingProvider } from './hashing-provider';
+
 import { SignInDto } from '../signInDto';
+
 import { GenerateTokensProvider } from './generate-tokens-provider';
 
 @Injectable()
@@ -9,28 +16,37 @@ export class SignInProvider {
   constructor(
     private readonly userService: UsersService,
     private readonly hashingProvider: HashingProvider,
-    private readonly generateTokensProvider: GenerateTokensProvider
+    private readonly generateTokensProvider: GenerateTokensProvider,
   ) {}
 
-  public signIn(signInDto: SignInDto) {
-    // Check if user exit
-    const user = this.userService.findByEmail(signInDto.email);
+  public async signIn(signInDto: SignInDto) {
+    // Check if user exists
+    const user = await this.userService.findByEmail(
+      signInDto.email,
+    );
 
     if (!user) {
-      throw new UnauthorizedException('email or password is incorrect');
+      throw new UnauthorizedException(
+        'Email or password is incorrect',
+      );
     }
 
-    // Compare the user password
-    let isComparePassword = false;
+    // Compare passwords
+    const isComparePassword =
+      await this.hashingProvider.comparePassword(
+        signInDto.password,
+        user.password,
+      );
 
-    try {
-      isComparePassword = this.hashingProvider.comparePassword(signInDto.password, user.);
-    } catch (error) {
-      throw new UnauthorizedException();
+    if (!isComparePassword) {
+      throw new UnauthorizedException(
+        'Email or password is incorrect',
+      );
     }
 
-    // generate tokens
-
-    return await this.generateTokensProvider.generateToken(user)
+    // Generate tokens
+    return await this.generateTokensProvider.generateToken(
+      user,
+    );
   }
 }
